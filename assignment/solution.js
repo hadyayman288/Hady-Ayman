28a8ced7b39888ddee9125ed710fc9ccb128cf6e
Hady <hady.abdelelmageed@student.giu-uni.de>
2026-09-22 14:44:39 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,12 @@
+const heroName = "Hady the Warrior";
+const studentId = "16001639";
+const power = String(Number(studentId) * 7);
+const warcry = '"By the Red Sea coast, the Ash Phoenix flees!"';
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
+
+
+
